e403a022b0ee36275deec32858ff57ef6c2f6160
Samuel Baafi Boakye <samuelbaafiboakye@gmail.com>
2024-09-03 22:14:08 +0000

    add blog model in domain package

---
+++ server/src/main/scala/com/sbboakye/blog/domain/Blog.scala
@@ -0,0 +1,21 @@
+package com.sbboakye.blog.domain
+
+import java.time.LocalDateTime
+
+case class Blog(
+    title: String,
+    content: String,
+    author: String,
+    created_date: LocalDateTime,
+    updated_date: LocalDateTime
+)
+
+object Blog {
+  val dummy: Blog = Blog(
+    "Hello World",
+    "Some helo world blog type",
+    "Sambeth",
+    LocalDateTime.now(),
+    LocalDateTime.now()
+  )
+}
