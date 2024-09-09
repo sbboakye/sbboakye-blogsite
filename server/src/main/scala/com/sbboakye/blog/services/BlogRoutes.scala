e09448d2becd45ac4cd980b11c3485fe75978695
Samuel Baafi Boakye <samuelbaafiboakye@gmail.com>
2024-09-09 11:06:11 +0000

    change blogservice to blogroutes

---
+++ server/src/main/scala/com/sbboakye/blog/services/BlogRoutes.scala
@@ -0,0 +1,47 @@
+package com.sbboakye.blog.services
+
+import cats.syntax.all.*
+import cats.effect.Sync
+import com.sbboakye.blog.views.BlogsView.*
+import org.http4s.dsl.Http4sDsl
+import org.http4s.*
+import org.http4s.scalatags.*
+import org.http4s.server.Router
+
+class BlogRoutes[F[_]: Sync] {
+
+  private val dsl: Http4sDsl[F] = new Http4sDsl[F] {}
+  import dsl.*
+
+  private val prefix = "/"
+
+  private val listViewRoute: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root =>
+    Ok(ListView.render)
+  }
+
+  private val detailViewRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> Root / UUIDVar(blogId) =>
+      Ok(DetailView.render)
+  }
+
+  private val createViewRoute: HttpRoutes[F] = HttpRoutes.of[F] { case POST -> Root / "create" =>
+    Ok(CreateView.render)
+  }
+
+  private val updateViewRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case PUT -> Root / UUIDVar(blogId) / "edit" =>
+      Ok(UpdateView.render)
+  }
+
+  private val deleteViewRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case DELETE -> Root / UUIDVar(blogId) / "delete" =>
+      Ok(DeleteView.render)
+  }
+
+  val routes: HttpRoutes[F] = Router(
+    prefix -> (listViewRoute <+> detailViewRoute <+> createViewRoute <+> updateViewRoute <+> deleteViewRoute)
+  )
+
+}
+
+object BlogService
